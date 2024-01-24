4107cfcf1fabf2fe10be41fe5a40619e1b78e918
AlviIslamAlamin <xtremealamin1999@gmail.com>
2024-01-24 14:44:23 +0600

    5th commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,39 @@ module.exports = {
     },
     plugins: [],
   }
+//   <aside>
+        
+//   <h4 class="text-2xl font-extrabold text-[#FFF] mb-5">Get In Touch</h4>
+//   <p class="mb-5">You can reach us by Phone, Email,<br> Post or on Social Media!</p>
+//   <div class="flex gap-4 mb-3">
+//     <img src="Images/Frame letter.png" alt="">
+//     <p>xtremealamin1999@gmail.com</p>
+//   </div>
+//   <div class="flex gap-4">
+//     <img src="Images/call.png" alt="">
+//     <p>01859*****</p>
+//   </div>
+// </aside> 
+
+// <nav>
+//   <h4 class=" text-white font-bold text-xl mb-10">Company</h4> 
+//   <a class="link link-hover">About us</a>
+//   <a class="link link-hover">Careers</a>
+//   <a class="link link-hover">Legal Notice</a>
+//   <a class="link link-hover">Leadership</a>
+//   <a class="link link-hover">News & Article</a>
+// </nav> 
+// <nav>
+//   <h4 class=" text-white font-extrabold text-xl mb-10">Support</h4> 
+//   <a class="link link-hover">Help Center</a>
+//   <a class="link link-hover">FAQ</a>
+//   <a class="link link-hover">Ticket Support</a>
+//   <a class="link link-hover">Contact Us</a>
+// </nav> 
+// <nav>
+//   <h4 class=" text-white font-extrabold text-xl mb-10">Services</h4> 
+//   <a class="link link-hover">Academy</a>
+//   <a class="link link-hover">Group Lesson</a>
+//   <a class="link link-hover">Private Lesson</a>
+//   <a class="link link-hover">Hockeys for kids</a>
+//   <a class="link link-hover">Hockeys for Adult</a>
